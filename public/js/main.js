13cb1f766255570d24f135e4d263c0a27214a8cf
Mike Lee <mike@traverstodd.com>
2020-03-24 12:08:41 -0700

    Initial Commit for full site

---
+++ public/js/main.js
@@ -0,0 +1,34 @@
+;(function () {
+	
+    'use strict';
+    var wp = document.getElementById('wp');
+    var needRaf = true;
+    var scrollHandler = function (event) {
+        var getRect = wp.getBoundingClientRect();
+        if(getRect.top < 200){ 
+            if(!wp.classList.contains('joined')){
+                wp.classList.add('joined');
+            }
+        } else {
+            if(wp.classList.contains('joined')){
+                wp.classList.remove('joined');
+            }
+        }
+    };
+    document.addEventListener('scroll', scrollHandler);
+
+    $(function(){
+        $(document)
+        .on('click', '.faq-header', function(){
+            var el = $(this);
+            if(el.hasClass('shown')){
+                el.removeClass('shown');
+                el.next().slideUp({duration: 500, easing: 'easeInOutQuart'});
+            } else {
+                el.addClass('shown');
+                el.next().slideDown({duration: 500, easing: 'easeInOutQuart'});
+            }
+        });
+    });
+
+}());
