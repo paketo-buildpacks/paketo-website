a8eeceaa033c80b76c32a8c124b28d188aec084a
Frankie Gallina-Jones <frankieg@vmware.com>
2021-06-08 18:34:17 -0400

    initial redesign of homepage
    - re-themes homepage
    - reorganizes and inlines homepage CSS
    - isolates homepage JavaScript
    - fills initial content
    
    Signed-off-by: Andy Brown <browna@vmware.com>

---
+++ static/v2/js/homepage.js
@@ -0,0 +1,39 @@
+;(function () {
+    'use strict';
+
+    // simple script to open external links in new tab
+    var links = document.getElementsByTagName("a");
+    for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+        if (links[i].hostname != window.location.hostname) {
+            links[i].target = '_blank';
+        }
+    }
+
+    var wp = document.getElementById('wp');
+    if(wp){ // don't bother if we're not on the homepage.
+        var scrollHandler = function (event) {
+            var getRect = wp.getBoundingClientRect();
+            if(getRect.top < 200){
+                if(!wp.classList.contains('joined')){
+                    wp.classList.add('joined'); }
+            } else {
+                if(wp.classList.contains('joined')){
+                    wp.classList.remove('joined');
+                }
+            }
+        };
+        document.addEventListener('scroll', scrollHandler);
+    }
+
+    $(function(){
+        $(document)
+        .on('click', '.nav-opener', function(){
+            var el = $(this);
+            if(el.hasClass('nav-opener__bar--opened')){
+                el.removeClass('nav-opener__bar--opened').next('nav').removeClass('nav--showing');
+            } else {
+                el.addClass('nav-opener__bar--opened').next('nav').addClass('nav--showing');
+            }
+        });
+    });
+}());
