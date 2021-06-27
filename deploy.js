d0686f0bcbaa2418e19e53d85e42e5fa4bd1d242
Andy Brown <browna@vmware.com>
2021-06-26 23:31:40 -0700

    again

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 firebase = require('firebase-tools');
 
-module.exports = async ({ context, github } = {} ) => {
+module.exports = async ({ context, github ) => {
   const { GITHUB_HEAD_REF, FIREBASE_TOKEN} = process.env;
   // overkill... probably.
   const branch = GITHUB_HEAD_REF.split('/')
@@ -12,19 +12,17 @@ module.exports = async ({ context, github } = {} ) => {
     .channel
     .deploy(branch, { json: true, e: '24h', only: 'paketo-staging', token: FIREBASE_TOKEN})
     .then((data) => {
-      if (context !== undefined && github !== undefined) {
-        console.log(`Lets create a comment`)
-        const payload = {
-          issues_number: context.issue.number,
-          owner: context.repo.owner,
-          repo: context.repo.repo,
-          body: `PR deployed to: ${data.paketo-staging.url}`
-        }
-        console.log(`payload: ${payload}`);
-        github.issues.createComment(payload).then((result) => {
-          console.log(result);
-        });
+      console.log(`Lets create a comment`)
+      const payload = {
+        issues_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: `PR deployed to: ${data.paketo-staging.url}`
       }
+      console.log(`payload: ${payload}`);
+      github.issues.createComment(payload).then((result) => {
+        console.log(result);
+      });
       console.log(data);
     })
     .catch((err) => {
