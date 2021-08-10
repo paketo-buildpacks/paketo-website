1edddcafbe789d00df1dbefcb4503e4a1a443e97
Andy Brown <browna@vmware.com>
2021-08-10 13:29:31 -0700

    Configure GCP auth in GHA for use with firebase deployment

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 firebase = require('firebase-tools');
 
 module.exports = async ({ expires } = { expires: '1h'}) => {
-  const { GITHUB_HEAD_REF, FIREBASE_TOKEN} = process.env;
+  const { GITHUB_HEAD_REF } = process.env;
 
   const branch = GITHUB_HEAD_REF.split('/')
       .filter(item => item.trim().length > 0)
@@ -10,7 +10,7 @@ module.exports = async ({ expires } = { expires: '1h'}) => {
   firebase
     .hosting
     .channel
-    .deploy(branch, { json: true, expires, only: 'paketo-staging', token: FIREBASE_TOKEN})
+    .deploy(branch, { json: true, expires, only: 'paketo-staging'})
     .then((data) => {
       console.log(`PR DEPLOYED TO: ${data['paketo-staging'].url}`)
       console.log(`SITE EXPIRES AT: ${data['paketo-staging'].expireTime} (in ${expires})`)
