4711d3774eca6caf7ab9cd24513c4c253f12b404
Ryan Moran <155736+ryanmoran@users.noreply.github.com>
2021-08-11 16:20:47 -0700

    Moves staging deployment to paketo-buildpacks project (#252)

--- deploy.js
+++ deploy.js
@@ -10,10 +10,10 @@ module.exports = async ({ expires } = { expires: '1h'}) => {
   firebase
     .hosting
     .channel
-    .deploy(branch, { json: true, expires, only: 'paketo-staging'})
+    .deploy(branch, { json: true, expires, only: 'paketo-buildpacks-staging'})
     .then((data) => {
-      console.log(`PR DEPLOYED TO: ${data['paketo-staging'].url}`)
-      console.log(`SITE EXPIRES AT: ${data['paketo-staging'].expireTime} (in ${expires})`)
+      console.log(`PR DEPLOYED TO: ${data['paketo-buildpacks-staging'].url}`)
+      console.log(`SITE EXPIRES AT: ${data['paketo-buildpacks-staging'].expireTime} (in ${expires})`)
     })
     .catch((err) => {
       console.log(`deploy: ${err}`);
