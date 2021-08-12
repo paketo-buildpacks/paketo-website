4177a9752080d85dffc2e84bc32a9ff9900f356f
Andy Brown <browna@vmware.com>
2021-08-12 11:37:46 -0700

    Rename staging site so GHA don't think the name is a secret (#255)

--- deploy.js
+++ deploy.js
@@ -10,10 +10,10 @@ module.exports = async ({ expires } = { expires: '1h'}) => {
   firebase
     .hosting
     .channel
-    .deploy(branch, { json: true, expires, only: 'paketo-buildpacks-staging'})
+    .deploy(branch, { json: true, expires, only: 'paketo-stage'})
     .then((data) => {
-      console.log(`PR DEPLOYED TO: ${data['paketo-buildpacks-staging'].url}`)
-      console.log(`SITE EXPIRES AT: ${data['paketo-buildpacks-staging'].expireTime} (in ${expires})`)
+      console.log(`PR DEPLOYED TO: ${data['paketo-stage'].url}`)
+      console.log(`SITE EXPIRES AT: ${data['paketo-stage'].expireTime} (in ${expires})`)
     })
     .catch((err) => {
       console.log(`deploy: ${err}`);
