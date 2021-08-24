9e7f268a4c8e3959c9b27a61562fb4ea3504d391
Frankie Gallina-Jones <frankieg@vmware.com>
2021-08-24 15:52:55 -0400

    add expander buttons;
    also tweak header and menu spacing/padding to align with mockup

--- assets/js/components/docs-sidebar.js
+++ assets/js/components/docs-sidebar.js
@@ -1,7 +1,7 @@
 export default class DocsSidebarExpander {
     constructor({ element }) {
         this.submenu = element;
-        this.expander = element.querySelector('.docs-menu__section-header')
+        this.expanderTriangle = element.querySelector('.expander-button__triangle')
         this.children = element.querySelector('.docs-menu__children-list');
 
         element.addEventListener('click', this.handleClick.bind(this));
@@ -10,8 +10,8 @@ export default class DocsSidebarExpander {
     handleClick(event) {
         console.log("Expander clicked!")
         if (this.submenu.querySelectorAll('.docs-menu__link--active').length == 0) {
-            this.expander.classList.toggle('docs-menu__section-header--opened')
             this.children.classList.toggle('docs-menu__children-list--opened')
+            this.expanderTriangle.classList.toggle('expander-button__triangle--opened')
         }
     }
   }
