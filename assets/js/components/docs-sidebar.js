252d47327ac2472c80e9c825aed8ba223152e4ab
Frankie Gallina-Jones <frankieg@vmware.com>
2021-08-25 12:16:03 -0400

    exclude submenu children from click handler

--- assets/js/components/docs-sidebar.js
+++ assets/js/components/docs-sidebar.js
@@ -1,14 +1,14 @@
 export default class DocsSidebarExpander {
     constructor({ element }) {
         this.submenu = element;
+        this.header = element.querySelector('.docs-menu__section-header')
         this.expanderTriangle = element.querySelector('.expander-button__triangle')
         this.children = element.querySelector('.docs-menu__children-list');
 
-        element.addEventListener('click', this.handleClick.bind(this));
+        this.header.addEventListener('click', this.handleClick.bind(this));
     }
 
     handleClick(event) {
-        console.log("Expander clicked!")
         if (this.submenu.querySelectorAll('.docs-menu__link--active').length == 0) {
             this.children.classList.toggle('docs-menu__children-list--opened')
             this.expanderTriangle.classList.toggle('expander-button__triangle--opened')
