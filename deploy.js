3c30577bbc66471ec81a6ee31c517daffcd0156a
TisVictress <vhenry@vmware.com>
2021-08-27 15:41:21 -0400

    remove GITHUB_HEAD_REF requirement (#290)
    * remove GITHUB_HEAD_REF requirement
    
    and consolidate workflows
    
    * add manual trigger to workflow
    
    Co-authored-by: Frankie Gallina-Jones <frankieg@vmware.com>

--- deploy.js
+++ deploy.js
@@ -3,14 +3,18 @@ firebase = require('firebase-tools');
 module.exports = async ({ expires } = { expires: '1h'}) => {
   const { GITHUB_HEAD_REF } = process.env;
 
-  const branch = GITHUB_HEAD_REF.split('/')
+  var channelID = "pr";
+
+  if (GITHUB_HEAD_REF) {
+    channelID = GITHUB_HEAD_REF.split('/')
       .filter(item => item.trim().length > 0)
       .pop();
+  }
 
   firebase
     .hosting
     .channel
-    .deploy(branch, { json: true, expires, only: 'paketo-stage'})
+    .deploy(channelID, { json: true, expires, only: 'paketo-stage'})
     .then((data) => {
       console.log(`PR DEPLOYED TO: ${data['paketo-stage'].url}`)
       console.log(`SITE EXPIRES AT: ${data['paketo-stage'].expireTime} (in ${expires})`)
