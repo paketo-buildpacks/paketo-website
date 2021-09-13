b1713827959f6f7d9ed9998155fb8d716ad18b8a
Frankie Gallina-Jones <frankieg@vmware.com>
2021-09-13 10:50:49 -0400

    move search bar out of nav expander

--- assets/js/components/docs_nav.js
+++ assets/js/components/docs_nav.js
@@ -1,11 +1,11 @@
 export default class DocsNav {
 	constructor({ element }) {
 	  this.toggle = element.querySelector('.docs-mobile-tab');
-	  this.sidebar = element.querySelector('.docs-sidebar');
+	  this.sidebar = element.querySelector('.docs-menu-container');
 	  this.toggle.addEventListener('click', this.handleClick.bind(this));
 	}
 
 	handleClick(event) {
-	  this.sidebar.classList.toggle('docs-sidebar--showing');
+	  this.sidebar.classList.toggle('docs-menu-container--showing');
 	}
       }
