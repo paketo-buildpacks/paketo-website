fe249e7237e29eed8ce15f9f4d76386c49b87f94
Frankie Gallina-Jones <frankieg@vmware.com>
2021-09-21 10:11:58 -0400

    bug fixes and shellcheck scripts

--- scripts/check-internal-links.sh
+++ scripts/check-internal-links.sh
@@ -3,9 +3,12 @@
 set -eu
 set -o pipefail
 
+# shellcheck source=SCRIPTDIR/.util/print.sh
+source "$(dirname "${BASH_SOURCE[0]}")/.util/print.sh"
+
 readonly PROGDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly SITEDIR="$(cd "${PROGDIR}/.." && pwd)"
-readonly CONTENTDIR="$(cd ${SITEDIR}/content && pwd)"
+readonly CONTENTDIR="$(cd "${SITEDIR}"/content && pwd)"
 
 function main() {
   while [[ "${#}" != 0 ]]; do
@@ -22,37 +25,33 @@ function main() {
         ;;
 
       *)
-        printf "unknown argument \"${1}\""
-        exit 1
+        util::print::error "unknown argument \"${1}\""
     esac
   done
 
   clean=0
 
-  refMessage="Internal links not wrapped in {{< ref >}} found:"
-  relrefMessage="Anchor links not wrapped in {{< relref >}} found:"
+  refMessage="Found internal links not wrapped in {{< ref >}} or {{< relref >}}:"
 
   if grep -irn "paketo.io" "${CONTENTDIR}" | grep -v "deps.paketo.io" | grep -qv "paketo.io/images" ; then
-    echo "${refMessage}";
+    util::print::title "${refMessage}";
     grep -irn "paketo.io" "${CONTENTDIR}" | grep -v "deps.paketo.io" | grep -v "paketo.io/images";
-    printf "\n\n"
     clean=1;
   fi
 
-  if grep -irnq "(/docs" "${CONTENTDIR}" || grep -irnq "(docs" ; then
-    echo "${refMessage}";
-    grep -irn "(/docs" "${CONTENTDIR}"
-    grep -irn "(docs" "${CONTENTDIR}"
-    printf "\n\n"
-    clean=1
-  fi
+  searchStrings=( "(/docs" "(docs" "(#" ":#" )
+  for search in "${searchStrings[@]}"; do
+    if grep -irnq "${search}" "${CONTENTDIR}"; then
+      if [ "${clean}" -eq 0 ]; then
+        util::print::title "${refMessage}"
+      fi
+      grep -irn "${search}" "${CONTENTDIR}";
+      clean=1
+    fi
+  done
 
-  if grep -irnq "(#" "${CONTENTDIR}" || grep -irnq ":#" "${CONTENTDIR}"; then
-    echo "${relrefMessage}";
-    grep -irn "(#" "${CONTENTDIR}"
-    grep -irn ":#" "${CONTENTDIR}"
-    printf "\n\n"
-    clean=1
+  if [ "${clean}" -eq 0 ]; then
+    util::print::success "Internal links are all wrapped."
   fi
 
   exit $clean
